57dd2a5938dc95d2dc1068695ae3d56ced57f5ef
anjelica <144828225+anjelica0224@users.noreply.github.com>
2026-03-10 21:36:27 +0530

    feat: v2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'anjelica';
 export const SITE_DESCRIPTION = 'welcome to my space!';
